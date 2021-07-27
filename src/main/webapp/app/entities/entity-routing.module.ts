dfc3d3f5511292f60d50467214c79631a4ad2064
yorez <yorez@naver.com>
2021-07-27 11:10:50 +0900

    feat: point add default

--- src/main/webapp/app/entities/entity-routing.module.ts
+++ src/main/webapp/app/entities/entity-routing.module.ts
@@ -4,6 +4,11 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'point',
+        data: { pageTitle: 'jhipsterMavenApp.point.home.title' },
+        loadChildren: () => import('./point/point.module').then(m => m.PointModule),
+      },
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ]),
   ],
